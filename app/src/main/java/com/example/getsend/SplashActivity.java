8a690df3f311b99325c847f0db2361e9a75a7249
chanaso <chanoshso20@gmail.com>
2019-11-14 15:56:59 +0200

    splash Screen

---
+++ app/src/main/java/com/example/getsend/SplashActivity.java
@@ -0,0 +1,48 @@
+package com.example.getsend;
+
+import android.content.Intent;
+import android.os.SystemClock;
+import android.os.Bundle;
+import android.widget.ProgressBar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity
+{
+    private static final int DELAY_TIME = 3; // seconds
+    private int timeLeft;
+    private ProgressBar progressBar;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        progressBar = findViewById(R.id.progressBarID);
+
+        startTimer();
+    }
+
+    private void startTimer()
+    {
+        timeLeft = DELAY_TIME;
+        new Thread(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                while(timeLeft >= 0)
+                {
+                    progressBar.setProgress(progressBar.getMax()-timeLeft);
+                    SystemClock.sleep(1000); //Thread.sleep(1000);
+                    timeLeft--;
+                }
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        }).start();
+    }
+}
+
