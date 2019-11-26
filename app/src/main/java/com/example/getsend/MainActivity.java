09bd966cf42017e10e371bb8665a179b2f48f6b1
Anael Zagoury <anael@protego.io>
2019-11-27 00:48:40 +0200

    add map to main activity

--- app/src/main/java/com/example/getsend/MainActivity.java
+++ app/src/main/java/com/example/getsend/MainActivity.java
@@ -2,13 +2,67 @@ package com.example.getsend;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.location.Location;
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+
+import com.mapbox.android.core.permissions.PermissionsListener;
+import com.mapbox.android.core.location.LocationEngineListener;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, View.OnClickListener, PermissionsListener, LocationEngineListener{
+
+    private MapView mapView;
+    private MapboxMap map;
+    private Button goBtn ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Mapbox.getInstance(this,"sk.eyJ1IjoiYW5hZWx6IiwiYSI6ImNrM2c2YnVtbDBiZ2QzaXAwcjM1ZjF3NWUifQ.8sjrOysub87S9Ic-ENb7qg");
         setContentView(R.layout.activity_main);
+        goBtn = findViewById(R.id.btnGo);
+        goBtn.setOnClickListener(this);
+        mapView = findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(this);
+
+    }
+
+    @Override
+    public void onConnected() {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onExplanationNeeded(List<String> permissionsToExplain) {
+
+    }
+
+    @Override
+    public void onPermissionResult(boolean granted) {
+
+    }
+
+    @Override
+    public void onMapReady(MapboxMap mapboxMap) {
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
