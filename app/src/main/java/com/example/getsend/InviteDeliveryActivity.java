4e4212a476724906ce1c9cd4d7f7e762ed20bbde
Anael Zagoury <anael@protego.io>
2020-03-16 17:41:14 +0200

    complite collecting of package info

--- app/src/main/java/com/example/getsend/InviteDeliveryActivity.java
+++ app/src/main/java/com/example/getsend/InviteDeliveryActivity.java
@@ -1,14 +1,45 @@
 package com.example.getsend;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class InviteDeliveryActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class InviteDeliveryActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private EditText edtxtWeight;
+    private EditText edtxtSize;
+    private EditText edtxtLocation;
+    private EditText edtxtDestination;
+    private Button btnEnter;
+    private FirebaseAuth mAuth;
+    private DatabaseReference ref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_invite_delivery);
+
+        edtxtWeight = findViewById(R.id.edtxt_weight);
+        edtxtSize = findViewById(R.id.edtxt_size);
+        edtxtLocation = findViewById(R.id.edtxt_location);
+        edtxtDestination = findViewById(R.id.edtxt_destination);
+        btnEnter = (Button) findViewById(R.id.btn_enter);
+
+        ref = FirebaseDatabase.getInstance().getReference().child("Package");
+        mAuth = FirebaseAuth.getInstance();
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        //edtxtWeight
+       // Package package = new Package(name, phone, pass, 4);
     }
 }
