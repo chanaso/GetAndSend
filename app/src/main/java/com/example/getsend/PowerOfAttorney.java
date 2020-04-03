8702757f7bf67c1138dbb10ce208411c8f54898f
Anael Zagoury <anael@protego.io>
2020-04-03 17:49:25 +0300

    Add power of attorney activity

---
+++ app/src/main/java/com/example/getsend/PowerOfAttorney.java
@@ -0,0 +1,13 @@
+package com.example.getsend;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PowerOfAttorney extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_power_of_attorney);
+    }
+}
