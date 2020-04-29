173311df8246ca3a676c8687f45e78f0b7dc9f2c
Anael Zagoury <anael@protego.io>
2020-04-29 05:16:08 +0300

    fix manifest

--- app/src/main/java/com/example/getsend/PickedPackagesListActivity.java
+++ app/src/main/java/com/example/getsend/PickedPackagesListActivity.java
@@ -1,32 +1,21 @@
 package com.example.getsend;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.TaskExecutors;
-import com.google.firebase.auth.PhoneAuthProvider;
-import com.google.firebase.database.ChildEventListener;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
-import com.google.gson.Gson;
 
 import java.util.ArrayList;
-import java.util.concurrent.TimeUnit;
 
 public class PickedPackagesListActivity extends AppCompatActivity {
     private String pickedPackageLocation;
