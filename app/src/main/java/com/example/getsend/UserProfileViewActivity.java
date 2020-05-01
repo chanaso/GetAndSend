27ea9c0fe4136d1e1d2d429f5632f34dbe7b9f1b
chanaso <chanoshso20@gmail.com>
2020-05-01 16:00:54 +0300

    profile user activity

---
+++ app/src/main/java/com/example/getsend/UserProfileViewActivity.java
@@ -0,0 +1,14 @@
+package com.example.getsend;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserProfileViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_profile_view);
+    }
+}
