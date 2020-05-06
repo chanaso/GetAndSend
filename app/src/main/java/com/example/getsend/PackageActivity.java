1b8e9462c929d67b63333e13b3ddf2a786841567
chanaso <chanoshso20@gmail.com>
2020-05-07 02:39:31 +0300

    fix bugs- delete chat works

--- app/src/main/java/com/example/getsend/PackageActivity.java
+++ app/src/main/java/com/example/getsend/PackageActivity.java
@@ -167,10 +167,6 @@ public class PackageActivity extends AppCompatActivity{
                         btn_1.setVisibility(View.INVISIBLE);
                         btn_2.setVisibility(View.INVISIBLE);
                         btn_confirm.setVisibility(View.INVISIBLE);
-
-                        //delete chat room
-                        DatabaseReference refChat = FirebaseDatabase.getInstance().getReference().child("ChatRooms");
-                        refChat.child("observable-"+packKey).removeValue();
                         break;
                 }
             }
@@ -238,6 +234,13 @@ public class PackageActivity extends AppCompatActivity{
                                 //Delivery confirmation
                                 rateUser(user2, pack.getPackageOwnerId());
                                 btn_confirm.setVisibility(View.INVISIBLE);
+
+                                //delete chat room
+                                DatabaseReference refChat = FirebaseDatabase.getInstance().getReference().child("ChatRooms");
+                                refChat.child("observable-"+packKey).removeValue();
+
+                                startActivity(new Intent(PackageActivity.this, NavbarPackagesActivity.class));
+                                finish();
                             }
                         });
                         break;
